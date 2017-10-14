7eddec12b62e98a260a7dd56c661543012751736
Eugene Kozlov <kozlovea8@gmail.com>
2017-10-14 16:18:14 +0300

    [fix] green rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
+# http://api.rubyonrails.org/classes/ActionController/Helpers.html
 module ApplicationHelper
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,4 @@
+# http://guides.rubyonrails.org/action_mailer_basics.html
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
