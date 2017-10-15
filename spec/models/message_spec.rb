4818219c72c9c74190f6d3e001d593e523606a82
Eugene Kozlov <kozlovea8@gmail.com>
2017-10-15 16:22:01 +0300

    [fix] remove unused specs

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -10,11 +10,4 @@ RSpec.describe Message, type: :model do
   describe 'ActiveRecord associations' do
     it { should belong_to(:user).counter_cache(:count_of_messages) }
   end
-
-  describe 'methods' do
-    let(:message) { create(:message) }
-    it 'return authors nickname ' do
-      expect(message.author_nickname).to equal(message.user.nickname)
-    end
-  end
 end
