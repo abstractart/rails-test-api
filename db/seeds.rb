dde85c2a63eda768698ef07ccdf24af52b205382
Eugene Kozlov <kozlovea8@gmail.com>
2018-07-19 18:34:29 +0300

    add random to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
-5.times do
-  user = User.create!(
+rand(5..10).times do
+  user = User.new(
     email: FFaker::Internet.email,
     nickname: FFaker::Name.name,
     password: FFaker::Internet.password(16)
   )
 
-  3.times do
+  rand(5..10).times do
     user.messages << Message.new(text: FFaker::Lorem.sentence(5))
   end
+  
+  user.save
 end
-User.all.each do |u|
-  (1..3).to_a.sample.times do
+
+User.find_each do |u|
+  rand(1..3).times do
     Vote.create(message: Message.order('random()').first, user: u)
   end
 end
